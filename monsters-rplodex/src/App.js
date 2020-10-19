76a92781effef0058600591c7753afe22d9e2088
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-10-19 22:14:13 +0530

    Updated the App file

--- monsters-rplodex/src/App.js
+++ monsters-rplodex/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>Don Jude,  Welcome to React</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            string: 'Hello Don Pulpally'
+        };
+    }
+
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            {this.state.string}
+          </p>
+          <button onClick={() => this.setState({string: 'Hello Sarath'})}> Change Text</button>
+          <button onClick={() => this.setState({string : 'Hello Friend'})}>Second button</button>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
