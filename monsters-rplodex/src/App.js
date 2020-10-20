410dce339df91083c63abfb0154f3829c100d060
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-10-20 22:27:10 +0530

    APP updated

--- monsters-rplodex/src/App.js
+++ monsters-rplodex/src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-    constructor(){
-        super();
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+      ],
+    };
+  }
 
-        this.state = {
-            string: 'Hello Don Pulpally'
-        };
-    }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters:users}));
+  }
+
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={() => this.setState({string: 'Hello Sarath'})}> Change Text</button>
-          <button onClick={() => this.setState({string : 'Hello Friend'})}>Second button</button>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name}</h1>
+        ))}
       </div>
     );
   }
